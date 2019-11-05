0574a7995e84fe55583ff385e62adc30a4327c91
eltiko <elticco@hotmail.com>
2019-11-05 10:34:35 -0800

    getting locations data

--- server.js
+++ server.js
@@ -8,9 +8,23 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/lab06-back', (request, response) =>{
-  response.send('data trom the /lab06-back route');
+  response.send('data from the /lab06-back route');
 });
 
+
+app.get('/location', (request, response) =>{
+  const geoData = require('./data/geo.json');
+  const city = request.query.data;
+  const locationData = new Location(city, geoData);
+  response.send(locationData);
+});
+
+function Location(city, geoData){
+  this.search_query = city;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
+}
 app.listen(PORT, () =>{
   console.log(`listening on PORT ${PORT}`);
 });
